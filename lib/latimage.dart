22a5680b674b6b76739cc3c8026f4e0898d3f50a
Rakha Tech <mrakha.tech@gmail.com>
2026-09-06 15:34:00 +0700

    added border to image

--- lib/latimage.dart
+++ lib/latimage.dart
@@ -5,6 +5,18 @@ const Latimage({ Key? key }) : super(key: key);
 
   @override
   Widget build(BuildContext context){
-    return Image.network("https://static.vecteezy.com/system/resources/thumbnails/033/494/777/small/cute-chibi-girl-wearing-a-cat-hoodie-ai-generative-png.png");
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        DecoratedBox(decoration: BoxDecoration(
+          border: Border.all(
+            color: Colors.black,
+            width: 5,
+          )
+        ),
+        child: Image.network("https://static.vecteezy.com/system/resources/thumbnails/033/494/777/small/cute-chibi-girl-wearing-a-cat-hoodie-ai-generative-png.png")
+        )
+      ],
+    );
   }
 }
